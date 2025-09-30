005346faeeeef47245e1fc939032c136914be73e
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-30 11:56:49 +0200

    Creation migration create_institutions -> warning the migration wasn't playing

---
+++ db/migrate/20250930095028_create_institutions.rb
@@ -0,0 +1,17 @@
+class CreateInstitutions < ActiveRecord::Migration[8.0]
+  def change
+    create_table :institutions do |t|
+      # 'citext' activé dans la première migration -> create_people
+      t.citext :slug, null: false
+      t.string :name, null: false
+      t.string :kind, null: false
+
+      t.timestamps
+    end
+
+    add_index :slug, null: false, unique: true
+    add_index :institutions, :name
+    add_index :institutions, :kind
+    add_index :institutions, [ :name, :kind ], unique: true
+  end
+end
