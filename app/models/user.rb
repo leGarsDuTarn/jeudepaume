e9ae14161198660726d60b323e930142c24e1c58
Benjamin Grassiano <grassiano.b@gmail.com>
2025-10-02 21:44:59 +0200

    Ajout enum pour role citoyen ou admin dans le model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Par default chaque inscrit est automatiquement un citoyen et non un admin
+  # -> voir migration add_tole_to_user
+  enum role: { citoyen: 0, admin: 1 }
 end
