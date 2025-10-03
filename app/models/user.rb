22aed7d204c829b2804e54372d02bfd1281590ff
Benjamin Grassiano <grassiano.b@gmail.com>
2025-10-03 20:08:05 +0200

    Rajout des colonnes user_name first_name & last_name a la table users -> done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Call back -> voir section private
   enum :role, { citoyen: 0, admin: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_02_193732) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_03_180650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -185,8 +185,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_02_193732) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0, null: false
+    t.string "user_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   add_foreign_key "assets_statements", "people"
